750673dcec94c19e544e10dcfaecd0f7063444d8
Pratham Mohan <pratham.parker14@gmail.com>
2026-06-08 22:24:52 +0530

    changed the port to 7860

--- app.py
+++ app.py
@@ -222,4 +222,4 @@ def analyze():
 if __name__ == "__main__":
     print("[RiskApp] Pre-loading model...")
     get_bundle()
-    app.run(debug=False, host="0.0.0.0", port=5000)
+    app.run(debug=False, host="0.0.0.0", port=7860)
